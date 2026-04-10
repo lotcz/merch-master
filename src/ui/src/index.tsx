e8e38c5683d0815518c7090ee438ae786dc73848
Karel Zavadil <zavadil@incomaker.com>
2026-04-10 16:46:13 +0200

    puzzle + vizitka

--- src/ui/src/index.tsx
+++ src/ui/src/index.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shared/style/general.less";
@@ -10,6 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
 	<StrictMode>
-		<App />
-	</StrictMode>,
+		<App/>
+	</StrictMode>
 );
